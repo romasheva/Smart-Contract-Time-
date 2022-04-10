c79f8bd293782a211ad2b3714d13ec8dabbc407a
Roman_Shevchenko <54140354+romasheva@users.noreply.github.com>
2022-04-10 09:57:05 +0300

    1

--- if-else.sol
+++ if-else.sol
@@ -46,5 +46,5 @@ contract SmeshSlim {
                 );
         }
 }
-                                                                                                                                                                                                                                                                                  
+                                                                                                                                                                                                                                                                                   
   
