af952aed2733af2ae35837ccce9313abfdf2937b
Roman_Shevchenko <54140354+romasheva@users.noreply.github.com>
2024-03-10 17:43:46 +0200

    1

--- TimeShop.sol
+++ TimeShop.sol
@@ -48,5 +48,4 @@ contract TimeShop{
             emit ItemFullyPaid(price, shopAddress);
         }
     } 
-}                                                                                            
- 
+}   
